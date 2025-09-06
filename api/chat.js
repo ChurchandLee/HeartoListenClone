cb745de77dbddf58c24d1d1f47f5b937f8f443aa
ChurchandLee <churchandlee@gmail.com>
2025-09-06 15:26:28 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,5 @@
-// /api/chat.js
+// api/chat.js
 export default async function handler(req, res) {
-  // Set CORS headers
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
